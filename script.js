29667db6bd732c250c205fbe03932871537b83a8
Pedro Vinicius <vinciidev05@gmail.com>
2024-02-20 00:27:01 -0300

    Working on CSS and HTML

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let apiKey = b735e147a6058d6e943d6887b68e44fc
-
-
-let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={12º16}&lon={38º58}&appid={b735e147a6058d6e943d6887b68e44fc}'
+let apiKey = 'b735e147a6058d6e943d6887b68e44fc';
+let city = 'Toronto, CA'
+let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=43.7001&lon=-79.4163&appid={b735e147a6058d6e943d6887b68e44fc}';
 
 fetch(apiUrl)
     .then(response => response.json())
@@ -9,8 +8,8 @@ fetch(apiUrl)
         const currentTemperature = data.main.temp;
         const currentConditions = data.weather[0].description;
 
-        document.getElementById('current-temperature').textContent = `Temperatura atual: ${currentTemperature}°C`;
-        document.getElementById('current-conditions').textContent = `Condições atuais: ${currentConditions}`;
+        document.getElementById('currentTemperature').textContent = `Temperatura atual: ${currentTemperature}°C`;
+        document.getElementById('currentConditions').textContent = `Condições atuais: ${currentConditions}`;
     })
     .catch(error => {
         console.error('Erro ao obter dados da API:', error);
