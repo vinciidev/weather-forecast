ec4e5c3e7d1388be9fa1e0e6d2f893fe07d6c4e4
Pedro Vinicius <vinciidev05@gmail.com>
2024-02-20 22:47:04 -0300

    Working on it

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 let apiKey = 'b735e147a6058d6e943d6887b68e44fc';
-let city = 'Toronto, CA'
-let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=43.7001&lon=-79.4163&appid={b735e147a6058d6e943d6887b68e44fc}';
+let city = 'toronto'
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const currentTemperature = data.main.temp;
-        const currentConditions = data.weather[0].description;
 
-        document.getElementById('currentTemperature').textContent = `Temperatura atual: ${currentTemperature}°C`;
-        document.getElementById('currentConditions').textContent = `Condições atuais: ${currentConditions}`;
-    })
-    .catch(error => {
-        console.error('Erro ao obter dados da API:', error);
-    });
+const res = await fetch(apiUrl);
+const data = await res.json;
+
+console.log(data)
